fce3e06579fa17889d3030ece4d72a538e1fec04
liya <liya@tikalk.com>
2011-03-18 08:56:38 +0200

    add job name validation

--- src/main/java/com/tikal/jenkins/plugins/multijob/PhaseJobsConfig.java
+++ src/main/java/com/tikal/jenkins/plugins/multijob/PhaseJobsConfig.java
@@ -82,27 +82,25 @@ public class PhaseJobsConfig implements
 		public AutoCompletionCandidates doAutoCompleteJobName(
 				@QueryParameter String value) {
 			AutoCompletionCandidates c = new AutoCompletionCandidates();
-			for (TopLevelItem job : Hudson.getInstance().getItems()) {
-				String localJobName=job.getName();
+			for (String localJobName : Hudson.getInstance().getJobNames()) {
 				if (localJobName.toLowerCase()
 						.startsWith(value.toLowerCase()))
-						c.add(job.getName());
+						c.add(localJobName);
 			}
 			return c;
 		}
+		
 		public FormValidation doCheckJobName(@QueryParameter String value) {
 			FormValidation result = FormValidation.errorWithMarkup("Invalid job name");
-			if (!value.isEmpty()) {
-				for (TopLevelItem job : Hudson.getInstance().getItems()) {
-					String localJobName=job.getName();
+			if (value.isEmpty()){
+				result = FormValidation.errorWithMarkup("Job name must not be empty");
+			} else {
+				for (String localJobName : Hudson.getInstance().getJobNames()) {
 					if (localJobName.toLowerCase()
 							.equals(value.toLowerCase()))
 						result=FormValidation.ok();
 				}
-			} else {
-				result=FormValidation.ok();
-			}
-			
+			} 
 			return result;
 		}
 		
