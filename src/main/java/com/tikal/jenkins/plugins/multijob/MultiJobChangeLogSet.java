d2d00ceab9075cf059d8f14f846b3131febad442
Itai <itai@tikalk.com>
2011-05-25 17:48:34 +0300

    Add MultiJobChangeSet

---
+++ src/main/java/com/tikal/jenkins/plugins/multijob/MultiJobChangeLogSet.java
@@ -0,0 +1,32 @@
+package com.tikal.jenkins.plugins.multijob;
+
+import java.util.Iterator;
+
+import hudson.model.AbstractBuild;
+import hudson.scm.ChangeLogSet;
+import hudson.scm.ChangeLogSet.Entry;
+
+public class MultiJobChangeLogSet extends ChangeLogSet<ChangeLogSet.Entry> {
+
+	protected MultiJobChangeLogSet(AbstractBuild build) {
+		super(build);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Iterator<hudson.scm.ChangeLogSet.Entry> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmptySet() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void addChangeLogSet(ChangeLogSet<? extends Entry> changeLogSet) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
