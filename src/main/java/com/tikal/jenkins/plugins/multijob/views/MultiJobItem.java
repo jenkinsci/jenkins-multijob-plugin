6f30525ef67bd4946cbfd463e71d0911a6c90dfe
Sergey Shelomentsev <sergey.shelomentsev@xored.com>
2016-05-12 20:59:30 +0600

    UI update

--- src/main/java/com/tikal/jenkins/plugins/multijob/views/MultiJobItem.java
+++ src/main/java/com/tikal/jenkins/plugins/multijob/views/MultiJobItem.java
@@ -52,20 +52,25 @@ public class MultiJobItem {
 			this.result = build.getResult();
 			this.statusIconColor = build.getIconColor().getImage();
 			this.lastDuration = build.getDurationString();
+			resolveLastCounts(project, build);
 		} else {
 			this.result = Result.NOT_BUILT;
 			this.statusIconColor = "nobuilt.png";
 			this.lastDuration = "N/A";
+			this.lastSuccess = "N/A";
+			this.lastFailure = "N/A";
 		}
 		this.status = null != this.result ? this.result.toString() : "Not built yet";
 		HealthReport health = project.getBuildHealth();
 		this.weather = health.getDescription();
 		this.weatherIconUrl = health.getIconUrl();
 		this.healthScore = health.getScore();
+		/*
 		this.lastSuccess = null != project.getLastSuccessfulBuild() ? project.getLastSuccessfulBuild()
 			.getTimestampString() : "N/A";
 		this.lastFailure = null != project.getLastFailedBuild() ? project.getLastFailedBuild()
 			.getTimestampString() : "N/A";
+			*/
 	}
 
 	public MultiJobItem(String name, Result result, String statusIconColor, String weather, String weatherIconUrl,
@@ -90,15 +95,15 @@ public class MultiJobItem {
 
 	private void resolveLastCounts(Job<?, ?> project, AbstractBuild<?, ?> build) {
 		Cause.UpstreamCause cause = build.getCause(Cause.UpstreamCause.class);
-		String prjStr = cause.getUpstreamProject();
 		if (null != cause) {
+			String prjStr = cause.getUpstreamProject();
 			int success = build.getResult().equals(Result.SUCCESS) ? build.getNumber() : 0;
 			int failure = build.getResult().equals(Result.FAILURE) ? build.getNumber() : 0;
 			boolean s = false;
 			boolean f = false;
 			for (Run run : project.getBuilds()) {
 				Cause.UpstreamCause c = (Cause.UpstreamCause) run.getCause(Cause.UpstreamCause.class);
-				if (c.getUpstreamProject().equals(prjStr)) {
+				if (null != c && c.getUpstreamProject().equals(prjStr)) {
 					if (run.getResult().equals(Result.SUCCESS) && run.getNumber() > success) {
 						success = run.getNumber();
 						s = true;
@@ -112,8 +117,8 @@ public class MultiJobItem {
 					break;
 				}
 			}
-			this.lastSuccess = 0 == success ? "N/A" : project.getBuildByNumber(success).getDurationString();
-			this.lastFailure = 0 == failure ? "N/A" : project.getBuildByNumber(failure).getDurationString();
+			this.lastSuccess = 0 == success ? "N/A" : project.getBuildByNumber(success).getTimestampString();
+			this.lastFailure = 0 == failure ? "N/A" : project.getBuildByNumber(failure).getTimestampString();
 		} else {
 			this.lastSuccess = null != project.getLastSuccessfulBuild() ? project.getLastSuccessfulBuild()
 					.getTimestampString() : "N/A";
