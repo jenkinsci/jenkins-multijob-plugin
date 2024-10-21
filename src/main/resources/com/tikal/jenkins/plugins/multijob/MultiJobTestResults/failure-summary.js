781a27c9351845a34a7cf896f213a843ff44299d
Shlomo Dahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-21 06:50:37 -0400

    [JENKINS-73965] Extract event handlers in `MultiJobTestResults/summary.jelly` (#375)
    * extract inline JS and address inline onClick handler

---
+++ src/main/resources/com/tikal/jenkins/plugins/multijob/MultiJobTestResults/failure-summary.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const showLink = document.getElementById("showLink");
+    if (showLink) {
+        showLink.addEventListener("click", showFailures);
+    }
+});
+
+function showFailures() {
+    var elms = document.getElementsByClassName("hidden");
+    for(var i = 0; i < elms.length; i++) {
+        elms[i].style.display = "";
+    }
+    elm = document.getElementById("showLink");
+    elm.style.display = "none";
+}
