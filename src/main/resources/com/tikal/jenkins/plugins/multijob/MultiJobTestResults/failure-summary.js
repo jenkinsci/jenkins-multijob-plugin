28a3219460e35586cd786edc26ae11f8c0c3b35c
Basil Crow <me@basilcrow.com>
2024-10-22 10:57:54 -0700

    Consistent formatting with other project PRs

--- src/main/resources/com/tikal/jenkins/plugins/multijob/MultiJobTestResults/failure-summary.js
+++ src/main/resources/com/tikal/jenkins/plugins/multijob/MultiJobTestResults/failure-summary.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const showLink = document.getElementById("showLink");
     showLink.addEventListener("click", showFailures);
 });
 
 function showFailures() {
     var elms = document.getElementsByClassName("hidden");
-    for(var i = 0; i < elms.length; i++) {
+    for (var i = 0; i < elms.length; i++) {
         elms[i].style.display = "";
     }
     elm = document.getElementById("showLink");
